c201442ab129acd9cca01022f2a80b8c2271a37a
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-27 03:41:17 +0300

    🚀feat(Difference) add Difference class
    This commit adds a new file called Difference.java which contains a class named Difference. This class includes a main method that takes input from the user and calculates the difference using the difference() method.

---
+++ src/main/java/org/Assiut/Difference.java
@@ -0,0 +1,21 @@
+package org.Assiut;
+
+import java.util.Scanner;
+
+public class Difference {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextInt(); 
+        long b = scanner.nextInt(); 
+        long c = scanner.nextInt();
+        long d = scanner.nextInt();
+        scanner.close();
+        System.out.println("Difference = " + difference(a, b, c, d));
+
+    }
+
+    public static long difference(long a, long b, long c, long d) {
+        return (a * b) - (c * d);
+    }
+   
+}
