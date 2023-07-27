e1979a74b9bdb34a8f0717fc3b50b426b43e98dc
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-27 03:22:14 +0300

    🚀feat(SayHello): add new Java class for saying hello

---
+++ src/main/java/org/Assiut/SayHello.java
@@ -0,0 +1,12 @@
+package org.Assiut;
+
+import java.util.Scanner;
+
+public class SayHello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name);
+        scanner.close();
+    }
+}
