3edea52f1953c85fd1a6f2e8bddd5ec5fd39e315
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-27 04:08:27 +0300

    🚀feat(Summation) add Summation class
    This commit adds the Summation class to the project. The Summation class accepts a number from the user and calculates the sum of all numbers from 1 to the given number using the formula n * (n + 1) / 2.

---
+++ src/main/java/org/Assiut/Summation.java
@@ -0,0 +1,17 @@
+package org.Assiut;
+
+import java.util.Scanner;
+
+public class Summation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        scanner.close();
+        System.out.println(summation(n));
+    }
+
+    private static long summation(long n) {
+        return n * (n + 1) / 2;
+    }
+}
+
