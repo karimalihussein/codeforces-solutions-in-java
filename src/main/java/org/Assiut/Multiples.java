d4876ae2287efdcaf04e12557b7c4d33229f9b87
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-27 05:58:44 +0300

    🚀feat(Multiples) add printMultiplesIfAMultipleOfB method

---
+++ src/main/java/org/Assiut/Multiples.java
@@ -0,0 +1,19 @@
+package org.Assiut;
+
+public class Multiples {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        scanner.close();
+        printMultiplesIfAMultipleOfB(a, b);
+    }
+
+    private static void print(String s) {
+        System.out.println(s);
+    }
+
+    public static void printMultiplesIfAMultipleOfB(long a, long b) {
+        print(a % b == 0 || b % a == 0 ? "Multiples" : "No Multiples");
+    }
+}
