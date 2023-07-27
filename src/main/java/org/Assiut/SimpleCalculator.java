d946af102fcd4be5621ca7c0adcc2309183eda30
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-27 03:36:12 +0300

    🚀feat(calculator) add SimpleCalculator class
    This commit adds a new class called SimpleCalculator which implements basic arithmetic operations such as addition, multiplication, and subtraction. It also includes a main method for user input and output.

---
+++ src/main/java/org/Assiut/SimpleCalculator.java
@@ -0,0 +1,29 @@
+package org.Assiut;
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextInt(); // 32 bit integer
+        long b = scanner.nextInt(); // 32 bit integer
+        scanner.close();
+        System.out.println(a + " + " + b + " = " + add(a, b));
+        System.out.println(a + " * " + b + " = " + multiply(a, b));
+        System.out.println(a + " - " + b + " = " + subtract(a, b));
+    }
+
+    public static long add(long a, long b) {
+        return a + b;
+    }
+
+    public static long multiply(long a, long b) {
+        return a * b;
+    }
+
+    public static long subtract(long a, long b) {
+        return a - b;
+    }
+
+}
