4e24c3c2adce56940301d95ef58cdb776f1e826c
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-27 03:45:49 +0300

    🚀feat(AreaOfACircle) add implementation to calculate the area of a circle

---
+++ src/main/java/org/Assiut/AreaOfACircle.java
@@ -0,0 +1,16 @@
+package org.Assiut;
+
+import java.util.Scanner;
+
+public class AreaOfACircle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double radius = scanner.nextDouble();
+        scanner.close();
+        System.out.println(area(radius));
+    }
+
+    public static double area(double radius) {
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
