2466c1e2a6014da699c49e071eb1017685a4984f
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-30 05:28:59 +0300

    🚀feat(sheets) add OneToN feature

---
+++ src/main/java/org/Assiut/SheetTwo/OneToN.java
@@ -0,0 +1,14 @@
+package org.Assiut.SheetTwo;
+
+import java.util.Scanner;
+
+public class OneToN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
