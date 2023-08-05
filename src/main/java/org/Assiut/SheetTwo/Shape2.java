cc018b82b1878718706841d3c97ce97a6755ed59
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-08-06 02:51:29 +0300

    work on Shape2

--- src/main/java/org/Assiut/SheetTwo/Shape2.java
+++ src/main/java/org/Assiut/SheetTwo/Shape2.java
@@ -3,16 +3,20 @@ package org.Assiut.SheetTwo;
 import java.util.Scanner;
 
 public class Shape2 {
-        public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-            int n = input.nextInt();
-            for(int i=1;i<=n;i++){
-                System.out.print(' ');
-                for(int j=1;j<=i;j++){
-                    System.out.print("*");
-                }
-                System.out.println();
+    public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+     int lines = input.nextInt();  
+     int spaces = lines - 1;
+     input.close();   
+        for (int i = 1; i <= lines; i++) {
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
             }
-            input.close();
-     }
+            spaces--;
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }   
+    }
 }
